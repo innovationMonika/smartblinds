5e5613bd4a3c03bbd4873f03b138b8ee674b66b5
Pragnesh <pragneshsuthar134@gmail.com>
2025-01-27 18:38:12 +0530

    Inline validation checkout js

--- app/code/GoMage/Checkout/view/frontend/requirejs-config.js
+++ app/code/GoMage/Checkout/view/frontend/requirejs-config.js
@@ -14,7 +14,10 @@ var config = {
         mixins: {
             'Magento_Checkout/js/action/set-shipping-information': {
                 'GoMage_Checkout/js/action/set-shipping-information-mixin': true
-            }
+            },
+            'Magento_Checkout/js/view/shipping-information': {
+                "GoMage_Checkout/js/view/inline-validation": true,
+            },
         }
     }
 };
